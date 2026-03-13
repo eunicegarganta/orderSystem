12495c9037ef58a220deccc1423f88746251e379
eunicegarganta <gargantaeunice1@gmail.com>
2026-03-13 19:48:39 +0800

    BL & DLL
    v1

---
+++ OrderData/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderData
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set;  }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+
+        public Order() { }
+        public Order(int orderId, string customerName, string productName, int quantity, string status)
+        {
+            OrderId = orderId;
+            CustomerName = customerName;
+            ProductName = productName;
+            Quantity = quantity;
+            Status = status;
+        }
+    }
+}
---
+++ OrderManager/OrderManager.cs
@@ -0,0 +1,60 @@
+﻿using OrderData;
+using OrderDataService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManager
+{
+    public class OrderManager
+    {
+        public OrderDataService dataService = new OrderDataService();
+
+        public List<Order> GetOrders()
+        {
+            return dataService.GetOrders();
+        }
+
+        public void CreateOrder(string customerName, string productName, int quantity)
+        {
+            int newId = dataService.GetOrders().Count + 1;
+
+            Order newOrder = new Order(newId, customerName, productName, quantity, "Pending");
+
+            dataService.Add(newOrder);
+        }
+
+        public void UpdateOrder(int id, string productName, int quantity)
+        {
+            Order? order = dataService.GetById(id);
+
+            if (order != null)
+            {
+                order.ProductName = productName;
+                order.Quantity = quantity;
+
+                dataService.Update(order);
+            }
+        }
+
+        public void DeleteOrder(int id)
+        {
+            dataService.Delete(id);
+        }
+
+        public void UpdateOrderStatus(int id, string status)
+        {
+            Order? order = dataService.GetById(id);
+
+            if (order != null)
+            {
+                order.Status = status;
+                dataService.Update(order);
+            }
+        }
+
+
+    }
+}
